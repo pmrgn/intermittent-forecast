498ced36f3240cd217cf8a20779d8c6465ccbb53
Paul Morgan <paulmorgan87@gmail.com>
2025-05-14 15:34:55 +1000

    Croston tests grouped

--- src/intermittent_forecast/base_forecaster.py
+++ src/intermittent_forecast/base_forecaster.py
@@ -5,9 +5,8 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, Any, TypeVar
 
-import numpy as np
-
 if TYPE_CHECKING:
+    import numpy as np
     import numpy.typing as npt
 
 T_BaseForecaster = TypeVar("T_BaseForecaster", bound="BaseForecaster")
@@ -20,7 +19,7 @@ class BaseForecaster(ABC):
     def fit(
         self: T_BaseForecaster,
         ts: npt.NDArray[np.float64],
-        *args: Any,
+        *args: Any,  # noqa: ANN401
         **kwargs: Any,  # noqa: ANN401
     ) -> T_BaseForecaster:
         """Fit the model to the time-series. Must be implemented."""
@@ -31,4 +30,4 @@ class BaseForecaster(ABC):
         start: int,
         end: int,
     ) -> npt.NDArray[np.float64]:
-        """Returns the forecast."""
+        """Return the forecast."""
